c1c9a48644e2979129365f76aba7a0c840c8d412
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-04-30 11:37:42 +0700

    (feat, view) add start and clear button

--- pages/index.jsx
+++ pages/index.jsx
@@ -48,7 +48,7 @@ export default function Home() {
         <div className="flex justify-center mb-2">
           <label className="mr-2" htmlFor="x-length">
             X Length (max 30, min 2){' '}
-            <input type="number" max={30} min={2} value={xInput} 
+            <input className="px-2 rounded" type="number" max={30} min={2} value={xInput} 
               onChange={(e) => {
                 if (e.target.value > 30) e.target.value = 30;
                 setXInput(e.target.value);
@@ -60,7 +60,7 @@ export default function Home() {
           </label>
           <label htmlFor="x-length" className="mr-2">
             Y Length (max 30, min 2){' '}
-            <input type="number" max={30} min={2} value={yInput} 
+            <input className="px-2 rounded" type="number" max={30} min={2} value={yInput} 
               onChange={(e) => {
                 if (e.target.value > 30) e.target.value = 30;
                 setYInput(e.target.value);
@@ -78,19 +78,19 @@ export default function Home() {
           >Generate</button>
         </div>
         <div className="flex gap-2 justify-center mb-2">
-          <button className={`bg-white rounded flex items-center p-2 ${pick === 'start' && 'border-2 border-black' }`}
+          <button className={`bg-white rounded flex items-center p-2 ${pick === 'start' && 'outline outline-2 outline-black' }`}
             onClick={() => dispatch(setPick('start'))}
           >
             <div className="bg-blue-700 w-5 h-5 mr-1"></div>
             <div>Pick Start</div>
           </button>
-          <button className={`bg-white rounded flex items-center p-2 ${pick === 'end' && 'border-2 border-black' }`}
+          <button className={`bg-white rounded flex items-center p-2 ${pick === 'end' && 'outline outline-2 outline-black' }`}
             onClick={() => dispatch(setPick('end'))}
           >
             <div className="bg-amber-500 w-5 h-5 mr-1"></div>
             <div>Pick End</div>
           </button>
-          <button className={`bg-white rounded flex items-center p-2 ${pick === 'wall' && 'border-2 border-black' }`}
+          <button className={`bg-white rounded flex items-center p-2 ${pick === 'wall' && 'outline outline-2 outline-black' }`}
             onClick={() => dispatch(setPick('wall'))}
           >
             <div className="bg-black w-5 h-5 mr-1"></div>
@@ -98,6 +98,12 @@ export default function Home() {
           </button>
         </div>
       </div>
+      
+      <div className="flex gap-2 justify-center mb-2">
+        <button className="bg-white rounded p-2 active:outline active:outline-2 active:outline-black">Start</button>
+        <button className="bg-white rounded p-2 active:outline active:outline-2 active:outline-black">Clear</button>
+      </div>
+
       <div ref={boardRef} id="board" className='grid justify-center	'>
         {boxes.length > 0 && boxes.map((b) => (
           <Box key={`${b.x}${b.y}${boardId}`} x={b.x} y={b.y} type={b.type} index={b.index} 
